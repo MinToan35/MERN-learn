abd4e3497c88ec0370d24fd24d52ffedeb9e4ae0
MinToan35 <mintoan351998@gmail.com>
2022-06-24 17:41:07 +0700

    end

---
+++ src/components/auth/Register.js
@@ -0,0 +1,100 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import AlertMessage from "../layout/Alert";
+const Register = () => {
+  //Context
+  const { registerUser } = useContext(AuthContext);
+
+  //localstage
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [alert, setAlert] = useState(null);
+
+  const { username, password, confirmPassword } = registerForm;
+  const onChangeRegisterForm = (event) => {
+    setRegisterForm({
+      ...registerForm,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const register = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setAlert({ type: "danger", message: "password do not match" });
+      setTimeout(() => setAlert(null), 5000);
+      return;
+    }
+    try {
+      const registerData = await registerUser(registerForm);
+
+      if (registerData.success) {
+      }
+      if (registerData.success) {
+      } else {
+        setAlert({ type: "danger", message: registerData.message });
+        setTimeout(() => setAlert(null), 5000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <AlertMessage info={alert}></AlertMessage>
+      <Form className="my-4" onSubmit={register}>
+        <Form.Group className="margin">
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
+            required
+            value={username}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Form.Group className="margin">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+            value={password}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Form.Group className="margin">
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            required
+            value={confirmPassword}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          Register
+        </Button>
+      </Form>
+      <p>
+        Already have an account?
+        <Link to="/login">
+          <Button variant="info" size="sm" className="ml-2">
+            Login
+          </Button>
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default Register;
